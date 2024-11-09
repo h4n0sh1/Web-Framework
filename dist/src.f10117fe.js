8be7451c82daee7ac1afa2c101463e92451c465f
h4n0s1 <hanchi.oth@gmail.com>
2024-11-09 20:45:17 +0100

    Testing it out

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -6220,6 +6220,10 @@ var user = new User_1.User({
   age: 100
 });
 console.log(user.get("name"));
+user.on("change", function () {
+  console.log("User was changed");
+});
+user.trigger("change");
 },{"./models/User":"src/models/User.ts"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
