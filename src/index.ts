e09fc8de2f149a2c3734241d6eacc8676abd15ac
h4n0s1 <hanchi.oth@gmail.com>
2024-11-09 20:31:31 +0100

    Implements passthrough methods

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: "new record", age: 100 });
 
-user.save();
-
-user.events.on("change", () => {
-  console.log("change !");
+user.on("change", () => {
+  console.log("User got changed!");
 });
 
-user.events.trigger("change");
+user.trigger("change");
