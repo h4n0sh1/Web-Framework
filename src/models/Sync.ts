c5a483cde16d6739d4f0d8d4451f915465c98519
h4n0s1 <hanchi.oth@gmail.com>
2024-11-09 15:11:11 +0100

    Turns Sync into a generic function

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,14 +1,17 @@
 import axios, { AxiosResponse, AxiosPromise } from "axios";
-import { UserProps } from "./User";
 
-export class Sync {
+interface HasId {
+  id: number;
+}
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     //ES2015 Destructuring <-> id = data.id
     const { id } = data;
 
