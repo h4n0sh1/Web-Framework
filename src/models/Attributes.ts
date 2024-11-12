0329a889fb3d11d8c05ffbb6ad2bbc6b160f8820
h4n0s1 <hanchi.oth@gmail.com>
2024-11-12 19:10:00 +0100

    Implementing Model.ts

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -2,7 +2,7 @@ export class Attributes<T extends object> {
   constructor(private data: T) {}
 
   // Constrains K to be one of the Keys declared in T
-  // Arrow function bounds this to the current instance
+  // Arrow function bounds "this" to the current instance
   get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
   };
